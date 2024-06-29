b2370f0005ce8ad9e540840569d129b3391884c3
Rafael Medeiros <rafaelmedeirosjob@gmail.com>
2024-06-29 15:49:51 -0300

    initial commit

---
+++ components/welcome-msg.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { useUser } from "@clerk/nextjs";
+
+export const WelcomeMsg = () => {
+  const { user, isLoaded } = useUser();
+
+  return (
+    <div className="space-y-2 mb-4">
+      <h2 className="text-2xl lg:text-4xl text-white font-medium">
+        Bem vindo {isLoaded ? ", " : " "}{user?.firstName} 👋🏻
+      </h2>
+      <p className="text-sm lg:text-base text-[#edf4ff]">
+        Administre e pontencialize seus ganhos com as menores taxas
+      </p>
+    </div>
+  );
+};
